79515b4795005a3a5eef2108216d834c043c689f
염규석 <yeumkyuseok@gmail.com>
2021-03-16 15:30:00 +0900

    added algorithm practice

---
+++ src/q9498/Main.java
@@ -0,0 +1,27 @@
+package q9498;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int result = sc.nextInt();
+
+        if(result >=90) {
+            System.out.println("A");
+        }
+            else if(result >= 80) {
+                System.out.println("B");
+        }
+            else if (result >= 70) {
+                System.out.println("C");
+        }
+            else if (result >= 60) {
+                System.out.println("D");
+        }
+            else {
+                System.out.println("F");
+        }
+    }
+}
