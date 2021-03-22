853238cd989892d17a894f1a6de674a91ff48e31
염규석 <yeumkyuseok@gmail.com>
2021-03-22 18:15:35 +0900

    added algorithm

---
+++ src/q2438/Main.java
@@ -0,0 +1,19 @@
+package q2438;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int j=0;
+
+        for(int i = 0 ; i < n ; i++) {
+            for(j = 0; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
