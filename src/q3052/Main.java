bb8f0415a0e3e80f955e891ab2662bf5e7ed70c3
염규석 <yeumkyuseok@gmail.com>
2021-03-29 04:03:34 +0900

    added algrorithm

--- src/q3052/Main.java
+++ src/q3052/Main.java
@@ -14,16 +14,21 @@ class Main {
             number[i] = input.nextInt()%42;
         }
 
-        int diff = 0;
-        for( int i = 0 ; i < 10 ; i++) {                //need an update on the algoithm
-            for(int j = 0; j < 10 ; j++) {
-                if (i != j) {
-                    if(number[i] !=  number[j]) {
-                        diff++;
+        int types = 1;
+
+        for( int i = 0 ; i < 9 ; i++) {                //need an update on the algoithm
+            boolean notContains = true;
+            for(int j = 9; j > i ; j--) {
+               if(i != j) {
+                    if (number[i] == number[j]) {
+                        notContains = false;
                     }
-                }
+               }
+            }
+            if (notContains == true) {
+                types++;
             }
         }
-        System.out.println(diff);
+        System.out.println(types);
     }
 }
