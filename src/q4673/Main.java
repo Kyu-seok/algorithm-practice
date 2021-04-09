090f427788252c484288fed19e387725fb61fd0f
kyu <yeumkyuseok@gmail.com>
2021-04-10 04:03:10 +0900

    added algorithm

--- src/q4673/Main.java
+++ src/q4673/Main.java
@@ -1,14 +1,33 @@
 package q4673;
 
 class Main {
+
+    static boolean[] array = new boolean[10001];
+
     public static void main(String args[]) {
 
+        int n;
+
+        for (int i = 1 ; i < 10001 ; i++) {
+            n = d(i);
+            if (n < 10001){
+                array[n] = true;
+            }
+        }
+
+        for (int i = 1 ; i < 10001 ; i++) {
+            if(array[i] != true) {
+                System.out.println(i);
+            }
+        }
     }
 
-    public int soulution(int number) {
-        int sum;
-
+    static public int d(int number) {
+        int sum = number;
+        while(number != 0) {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
         return sum;
-
     }
 }
