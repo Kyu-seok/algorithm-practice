1f9246edfbf39e8864df6e92e2f02aa7db9e700c
염규석 <yeumkyuseok@gmail.com>
2021-04-09 23:35:24 +0900

    added algorithm

--- src/q4673/Main.java
+++ src/q4673/Main.java
@@ -2,6 +2,13 @@ package q4673;
 
 class Main {
     public static void main(String args[]) {
-        
+
+    }
+
+    public int soulution(int number) {
+        int sum;
+
+        return sum;
+
     }
 }
