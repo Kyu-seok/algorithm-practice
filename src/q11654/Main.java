73d90d6d8b9da6b970b2e0cc9f9a58bf895fbc39
kyu <yeumkyuseok@gmail.com>
2021-04-11 07:04:43 +0900

    added algorithm

---
+++ src/q11654/Main.java
@@ -0,0 +1,13 @@
+package q11654;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        char character = input.next().charAt(0);
+
+        System.out.println((int)character);
+    }
+}
