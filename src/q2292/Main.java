de28058a5f64afb77bfed11567467cd8f2b24ba6
염규석 <yeumkyuseok@gmail.com>
2021-05-08 12:51:28 +0900

    added algorithm

--- src/q2292/Main.java
+++ src/q2292/Main.java
@@ -8,30 +8,18 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         int x = input.nextInt();
+        int count = 1;
+        int range = 2;
 
-        float n = ((float) x-1)/6;
-        int result = 0;
-        if(n == 0) {
-            result = 1;
+        if(x==1) {
+            System.out.println("1");
         }
-        else if((n>0)&&(n<=1)) {
-            result = 2;
+        else {
+             while(range <= x) {
+                 range = range + (count * 6);
+                 count++;
+             }
+             System.out.println(count);
         }
-        else if((n>1)&&(n<=3)) {
-            result = 3;
-        }
-        else if((n>3)&&(n<=6)) {
-            result = 4;
-        }
-        else if((n>6)&&(n<=10)) {
-            result = 5;
-        }
-        else if((n>10)&&(n<=15)) {
-            result = 6;
-        }
-        else if((n>15)&&(n<=21)) {
-            result = 7;
-        }
-        System.out.println(result);
     }
 }
