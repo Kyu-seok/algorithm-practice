585f51bb94ccb33caace6e3ca5217b54f2389dbf
Kyu-seok <yeumkyuseok@gmail.com>
2022-06-26 01:36:28 +0900

    Solve 1. Two Sum

---
+++ LeetCode/src/_1_Two_Sum/Main.java
@@ -0,0 +1,10 @@
+package _1_Two_Sum;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] num = {3,3};
+        int[] result = solution.twoSum(num, 6);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
